59ccb02bd9184fcea6d2fde2a5c6c97c07412350
vy <50475727+viviRbi@users.noreply.github.com>
2020-12-03 23:12:09 -0600

    Add hero and message service to share infomation between them

---
+++ src/app/hero.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Hero} from './interfaces/hero';
+import { HEROES } from './mock-heroes';
+import {MessageService} from './message.service';
+
+import {Observable, of} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]>{
+    this.messageService.add('HeroService: fetch heroes');
+    return of(HEROES);
+  }
+
+}
